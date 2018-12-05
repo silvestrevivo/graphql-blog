d7c43f078173d0ad80074e08a4427a36c1c9c67e
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-05 16:28:18 +0100

    creating models with mongoose.schema

--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' })
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
 
 // Connect to data base
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('connected to DB Atlas'))
   .catch(err => console.error(err))
+mongoose.set('useCreateIndex', true)
 
 // Initializes application
 const app = express();
