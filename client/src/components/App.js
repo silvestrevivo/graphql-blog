291d971d1e254fdbca930623b9655d8cf11fd619
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-06 09:26:02 +0100

    first query with apollo client, query commpent and css style added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Query } from 'react-apollo';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Home
-      </div>
-    );
-  }
+import { GET_ALL_RECIPES } from './queries'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Home</h1>
+      <Query query={GET_ALL_RECIPES}>
+        {
+          ({ data, loading, error }) => {
+            if (loading) return <div>Loading...</div>
+            if (error) return <div>Error</div>
+            console.log('data', data)
+            return (
+              <p>Recipes</p>
+            )
+          }
+        }
+      </Query>
+    </div>
+  )
 }
 
 export default App;
