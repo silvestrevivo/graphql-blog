dc378a20fac57c6010fe8e78836b8b42e5f5a06c
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-15 12:53:21 +0100

    adding react-router-dom and path for authentication

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import './index.css';
 import App from './components/App';
+import Signin from './components/Auth/Signin';
+import Signup from './components/Auth/Signup';
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -10,8 +13,19 @@ const client = new ApolloClient({
   uri: 'http://localhost:4444/graphql'
 })
 
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/signin" component={Signin} exact />
+      <Route path="/signup" component={Signup} exact />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+)
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    {Root()}
   </ApolloProvider>
   , document.getElementById('root'));
